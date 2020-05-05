f8ae5d994a94b3c12eeff642f31d3fe8864988c8
Yernar Abilbay <eabilbay@gmail.com>
2020-05-05 22:32:31 +0600

    readme pngs added.

--- src/App.js
+++ src/App.js
@@ -213,55 +213,58 @@ class App extends Component {
                     </g>
                 </g>
             </svg>
-        } else if (this.state.emoji = 'fine') {
-            return <svg className="fine" width="44px" height="44px" viewBox="0 0 44 44" version="1.1"
-                        xmlns="http://www.w3.org/2000/svg">
-                <g id="fine-emotion" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                    <g id="fine">
-                        <circle id="body" fill="#1988E3" cx="22" cy="22" r="22"></circle>
-                        <g className="matrix" transform="translate(22.000000, 32.000000)">
-                            <g className="face-container">
-                                <g className="face" transform="translate(-9, -12)">
-                                    <g className="face-upAndDown">
-                                        <g className="eyes">
-                                            <ellipse className="right-eye" fill="#2C0E0F" cx="16.0941176"
-                                                     cy="1.75609756" rx="1.90588235" ry="1.75609756"></ellipse>
-                                            <ellipse className="left-eye" fill="#2C0E0F" cx="1.90588235" cy="1.75609756"
-                                                     rx="1.90588235" ry="1.75609756"></ellipse>
-                                        </g>
-                                        <path
-                                            d="M6.18823529,4.90499997 C6.18823529,5.95249999 7.48721095,7 9.08957864,7 C10.6919463,7 11.990922,5.95249999 11.990922,4.90499997"
-                                            id="mouth" stroke="#2C0E0F" stroke-linecap="round"
-                                            stroke-linejoin="round"></path>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-        } else if (this.state.emoji = 'happy') {
-            return <svg className="happy" width="44px" height="44px" viewBox="0 0 44 44" version="1.1"
-                        xmlns="http://www.w3.org/2000/svg">
-                <g id="Happy" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
-                   transform="translate(0, 0)">
-                    <circle id="Body" fill="#248C37" cx="22" cy="22" r="22"></circle>
-                    <g className="scaleFace">
-                        <g className="face">
-                            <ellipse id="Eye-right" fill="#2C0E0F" cx="29.0875" cy="21.75" rx="1.89926471"
-                                     ry="1.75"></ellipse>
-                            <ellipse id="Eye-left" fill="#2C0E0F" cx="14.8992647" cy="21.75" rx="1.89926471"
-                                     ry="1.75"></ellipse>
-                            <path
-                                d="M21.8941176,27.8819633 C24.8588235,27.8819632 25.4941176,25.5404999 25.4941176,24.5648901 C25.4941176,23.5892803 24.4352941,23.9795242 22.1058824,23.9795242 C19.7764706,23.9795242 18.2941176,23.5892803 18.2941176,24.5648901 C18.2941176,25.5404999 18.9294118,27.8819633 21.8941176,27.8819633 Z"
-                                id="Mouth" fill="#2C0E0F"></path>
-                            <ellipse id="Tung" fill="#E23D18" cx="21.8941176" cy="26.4390244" rx="1.69411765"
-                                     ry="0.780487805"></ellipse>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-        } else if (this.state.emoji = 'sad') {
+        }
+            // else if (this.state.emoji = 'fine') {
+            //     return <svg className="fine" width="44px" height="44px" viewBox="0 0 44 44" version="1.1"
+            //                 xmlns="http://www.w3.org/2000/svg">
+            //         <g id="fine-emotion" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            //             <g id="fine">
+            //                 <circle id="body" fill="#1988E3" cx="22" cy="22" r="22"></circle>
+            //                 <g className="matrix" transform="translate(22.000000, 32.000000)">
+            //                     <g className="face-container">
+            //                         <g className="face" transform="translate(-9, -12)">
+            //                             <g className="face-upAndDown">
+            //                                 <g className="eyes">
+            //                                     <ellipse className="right-eye" fill="#2C0E0F" cx="16.0941176"
+            //                                              cy="1.75609756" rx="1.90588235" ry="1.75609756"></ellipse>
+            //                                     <ellipse className="left-eye" fill="#2C0E0F" cx="1.90588235" cy="1.75609756"
+            //                                              rx="1.90588235" ry="1.75609756"></ellipse>
+            //                                 </g>
+            //                                 <path
+            //                                     d="M6.18823529,4.90499997 C6.18823529,5.95249999 7.48721095,7 9.08957864,7 C10.6919463,7 11.990922,5.95249999 11.990922,4.90499997"
+            //                                     id="mouth" stroke="#2C0E0F" stroke-linecap="round"
+            //                                     stroke-linejoin="round"></path>
+            //                             </g>
+            //                         </g>
+            //                     </g>
+            //                 </g>
+            //             </g>
+            //         </g>
+            //     </svg>
+            // }
+            // else if (this.state.emoji = 'happy') {
+            //     return <svg className="happy" width="44px" height="44px" viewBox="0 0 44 44" version="1.1"
+            //                 xmlns="http://www.w3.org/2000/svg">
+            //         <g id="Happy" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
+            //            transform="translate(0, 0)">
+            //             <circle id="Body" fill="#248C37" cx="22" cy="22" r="22"></circle>
+            //             <g className="scaleFace">
+            //                 <g className="face">
+            //                     <ellipse id="Eye-right" fill="#2C0E0F" cx="29.0875" cy="21.75" rx="1.89926471"
+            //                              ry="1.75"></ellipse>
+            //                     <ellipse id="Eye-left" fill="#2C0E0F" cx="14.8992647" cy="21.75" rx="1.89926471"
+            //                              ry="1.75"></ellipse>
+            //                     <path
+            //                         d="M21.8941176,27.8819633 C24.8588235,27.8819632 25.4941176,25.5404999 25.4941176,24.5648901 C25.4941176,23.5892803 24.4352941,23.9795242 22.1058824,23.9795242 C19.7764706,23.9795242 18.2941176,23.5892803 18.2941176,24.5648901 C18.2941176,25.5404999 18.9294118,27.8819633 21.8941176,27.8819633 Z"
+            //                         id="Mouth" fill="#2C0E0F"></path>
+            //                     <ellipse id="Tung" fill="#E23D18" cx="21.8941176" cy="26.4390244" rx="1.69411765"
+            //                              ry="0.780487805"></ellipse>
+            //                 </g>
+            //             </g>
+            //         </g>
+            //     </svg>
+            // }
+        else if (this.state.emoji = 'sad') {
             return <svg className="sad" width="44px" height="44px" viewBox="0 0 44 44" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                 <g id="sad" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(0, 0)">
